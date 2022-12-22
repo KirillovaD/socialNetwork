f7cf2606c487afec49be150c36e58ddacf8af688
KirillovaD <sdaria@inbox.ru>
2022-12-22 12:21:25 +0300

    add messages

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,9 +3,21 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <>
-            Dialogs
-        </>
+        <div className={s.dialogs}>
+          <div className={s.dialogs_items}>
+              <div className={s.dialog +' '+ s.active}>Dmitrii</div>
+              <div className={s.dialog}>Kate</div>
+              <div className={s.dialog}>Svetlana</div>
+              <div className={s.dialog}>Alex</div>
+              <div className={s.dialog}>Nick</div>
+          </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+            </div>
+
+        </div>
     );
 };
 
